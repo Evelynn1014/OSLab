aef44bd12cc4240c819cde57fdc5e3958ad187d2
Evelynn1014 <1371894384@qq.com>
2022-12-10 17:02:02 +0800

    20301107 刘佳垚 16:58 lab6-3

--- os/src/main.rs
+++ os/src/main.rs
@@ -15,6 +15,7 @@ mod config;
 mod task;
 mod timer;
 mod mm;
+
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
 
@@ -48,4 +49,24 @@ pub fn rust_main() -> ! {
 #[alloc_error_handler]
 pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
     panic!("Heap allocation error, layout = {:?}", layout);
+}
+
+#[macro_use]
+extern crate bitflags;
+
+// os/src/mm/page_table.rs
+
+use bitflags::*;
+
+bitflags! {
+    pub struct PTEFlags: u8 {
+        const V = 1 << 0;
+        const R = 1 << 1;
+        const W = 1 << 2;
+        const X = 1 << 3;
+        const U = 1 << 4;
+        const G = 1 << 5;
+        const A = 1 << 6;
+        const D = 1 << 7;
+    }
 }
