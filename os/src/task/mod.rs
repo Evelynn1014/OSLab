b5fad4e61270a81c2003c93331a3a0feacc6468a
Evelynn1014 <1371894384@qq.com>
2022-12-19 19:05:53 +0800

    20301107 刘佳垚 19:05 lab7-9

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -13,8 +13,34 @@ use alloc::sync::Arc;
 use manager::fetch_task;
 
 pub use context::TaskContext;
+pub use processor::{
+    run_tasks,
+    current_task,
+    current_user_token,
+    current_trap_cx,
+    take_current_task,
+    schedule,
+};
+pub use manager::add_task;
+pub use pid::{PidHandle, pid_alloc, KernelStack};
 
+pub fn suspend_current_and_run_next() {
+    // There must be an application running.
+    let task = take_current_task().unwrap();
 
+    // ---- access current TCB exclusively
+    let mut task_inner = task.inner_exclusive_access();
+    let task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
+    // Change status to Ready
+    task_inner.task_status = TaskStatus::Ready;
+    drop(task_inner);
+    // ---- release current PCB
+
+    // push back to ready queue.
+    add_task(task);
+    // jump to scheduling cycle
+    schedule(task_cx_ptr);
+}
 
 lazy_static! {
     pub static ref INITPROC: Arc<TaskControlBlock> = Arc::new(
