e77e82a7ba4786edaf0b2c595d5fc74945735d80
Evelynn1014 <1371894384@qq.com>
2022-12-19 21:46:07 +0800

    20301107 刘佳垚  lab7 勘误后

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -1,4 +1,8 @@
 const SYSCALL_READ: usize = 63;
+const SYSCALL_WRITE: usize = 64;
+const SYSCALL_EXIT: usize = 93;
+const SYSCALL_YIELD: usize = 124;
+const SYSCALL_GET_TIME: usize = 169;
 const SYSCALL_GETPID: usize = 172;
 const SYSCALL_FORK: usize = 220;
 const SYSCALL_EXEC: usize = 221;
@@ -9,6 +13,7 @@ mod process;
 
 use fs::*;
 use process::*;
+
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
         SYSCALL_READ => sys_read(args[0], args[1] as *const u8, args[2]),
